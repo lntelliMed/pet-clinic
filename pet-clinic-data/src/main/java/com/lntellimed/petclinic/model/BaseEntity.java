172d9059cdf32218781047be6a4cb64dbe2762ca
“Sam <sam.alsmadi@gmail.com>
2018-11-27 22:01:33 -0500

    Update base entity with JPA annotations

--- pet-clinic-data/src/main/java/com/lntellimed/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/lntellimed/petclinic/model/BaseEntity.java
@@ -2,15 +2,22 @@ package com.lntellimed.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private Long id;
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
