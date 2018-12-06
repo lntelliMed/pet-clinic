a952a2968d3a5b1aa794d02a188c0907a821c400
“Sam <sam.alsmadi@gmail.com>
2018-12-05 22:02:33 -0500

    Add spring data jpa service for pet types

--- pet-clinic-data/src/main/java/com/lntellimed/petclinic/repositories/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/com/lntellimed/petclinic/repositories/PetTypeRepository.java
@@ -2,6 +2,8 @@ package com.lntellimed.petclinic.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.lntellimed.petclinic.model.PetType;
+
 
 public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
