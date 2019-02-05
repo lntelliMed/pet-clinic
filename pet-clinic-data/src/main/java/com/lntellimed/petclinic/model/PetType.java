187ff0f55a7fded862f83dc25779daa09e61c765
“Sam <sam.alsmadi@gmail.com>
2019-02-04 22:08:23 -0500

    Add builder patter to PetType

--- pet-clinic-data/src/main/java/com/lntellimed/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/lntellimed/petclinic/model/PetType.java
@@ -16,15 +16,20 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 	@Column(name = "name")
 	private String name;
-	
+
+	@Builder
+	public PetType(Long id, String name) {
+		super(id);
+		this.name = name;
+	}
+
 	@Override
-    public String toString() {
-        return name;
-    }
+	public String toString() {
+		return name;
+	}
 }
