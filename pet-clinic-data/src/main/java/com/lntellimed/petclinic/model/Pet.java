e641b6ddf1da06b8965f6b21b65f2550d258e51c
“Sam <sam.alsmadi@gmail.com>
2019-02-04 22:24:49 -0500

    Allow users to add and update pets

--- pet-clinic-data/src/main/java/com/lntellimed/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/lntellimed/petclinic/model/Pet.java
@@ -24,10 +24,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
+	@Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
+    }
 	@Column(name = "name")
 	private String name;
 
